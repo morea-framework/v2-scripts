74973f3439af7a101792cca72dad28a6227d6027
Philip Johnson <johnson@hawaii.edu>
2014-05-11 05:59:11 -1000

    Adding the mores-pull script

---
+++ morea-pull.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -d "./master" ]; then
+  echo "master/ directory does not exist.  Exiting..."
+  exit 1
+fi
+
+if [ ! -d "./gh-pages" ]; then
+  echo "gh-pages/ directory does not exist.  Exiting..."
+  exit 1
+fi
+
+echo "Pulling the gh-pages directory."
+( set -x ; cd ./gh-pages ; git pull ) 
+
+echo "Pulling the master directory"
+( set -x ; cd ./master ; git pull)
+
