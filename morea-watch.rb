0101961e4c0d113da31548ef25e4cae96a8e9412
Dan Port <dport96@gmail.com>
2015-10-10 13:25:01 -1000

    Enchancements and forking bug fix.
    Fixed restarting morea-run-local only when if it is already running.
    Added default watch to ./master/src/morea if no ARGV. Added check for
    Windows as this won’t work on Windows.

--- morea-watch.rb
+++ morea-watch.rb
@@ -1,35 +1,43 @@
 #!/usr/bin/env ruby
-# morea-watch.rb dport@hawaii.edu
+# morea-watch.rb dport@hawaii.edu 
 # Adapted from watch.rb by Brett Terpstra, 2011 <http://brettterpstra.com>
 # with credit to Carlo Zottmann <https://github.com/carlo/haml-sass-file-watcher>
-#
+# 
 # This file must be in the same directory as morea-run-local.sh
 #
-# *Warning* If you watch a folder (or subfolder) that jekyll generates files in when run in it may unpleasantly
-# stop and re-start for each generated file since we don't check when output is completed
+# *Warning* If you watch a folder (or subfolder) that jekyll generates files in when run in it may unpleasantly 
+# stop and re-start for each generated file since we don't check when output is completed 
 # after forking morea-run-local (probably should do this though...)
 #
+
+if RUBY_PLATFORM =~ /mswin32/ then
+ puts "Sorry, morea-watch does not work on Windows"
+ exit
+end
 
 def fork_MOREA()
   pid = fork do
     exec "./morea-run-local.sh"
   end
-  Process.detach(pid)
-  sleep 5
-  return pid
+	Process.detach(pid)
+	sleep 5
+	return pid 
 end
 
 trap("SIGINT") { exit }
 
-if ARGV.length < 1
+if ARGV.length > 1
   puts "Usage: #{$0} watch_folder"
   puts "Example: #{$0} ./master/src/morea"
   exit
+elseif ARGV.length == 1
+  watch_folder = watch_folder = ARGV[0]
+else 
+  watch_folder = "./master/src/morea"
 end
 
 filetypes = ['css','html','htm', 'js', 'md']
-watch_folder = ARGV[0]
-keyword = ARGV[1]
+
 
 morea_pid = fork_MOREA
 
@@ -46,13 +54,13 @@ while true do
 
   unless diff_hash.empty?
     hash = new_hash
-
+    
     diff_hash.each do |df|
       puts "Detected change in #{df[0]}"
     end
-
+    
     unless `pgrep -f .*morea-run-local.*` == ""
-
+    
       base=Process.pid
       descendants = Hash.new{|ht,k| ht[k]=[k]}
       Hash[*`ps -eo pid,ppid`.scan(/\d+/).map{|x|x.to_i}].each{|pid,ppid|
@@ -68,12 +76,10 @@ while true do
           next
         end
       end
-
       Process.waitall
       puts "goodby pid #{morea_pid}, restarting MOREA"
-      morea_pid = fork_MOREA
     end
-
+    morea_pid = fork_MOREA
   end
   sleep 1
 end
